d511c3497748272371faed36a2e4c7ad98f52d74
XFaon <xfaonae@protonmail.com>
2021-12-26 22:03:03 -0500

    Chore: Init

---
+++ server/src/main.ts
@@ -0,0 +1,3 @@
+console.log("  ReFlux Framework Issue. Waiting for a release. \n  Please clone this server again later when ReFlux has\n  been released and commits have been made to the server.");
+console.log("  Sorry :(");
+console.log("  - The Skylix Development Team");
