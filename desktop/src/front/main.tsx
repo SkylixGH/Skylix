803d6fe59b794407f46c38b4903dd83b9d32ca7d
XFaon <xfaonae@protonmail.com>
2022-01-08 23:48:00 -0500

    App

--- desktop/src/front/main.tsx
+++ desktop/src/front/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "@skylixgh/luxjs-client";
+import { App, Button, theming, themingThemes } from "@skylixgh/luxjs-client";
+
+theming.installTheme(themingThemes.defaultDarkTheme).then(() => {
+    theming.loadTheme(themingThemes.defaultDarkTheme.author, themingThemes.defaultDarkTheme.name);
+});
 
 ReactDOM.render(
-    <App>Hello</App>,
+    <App>
+        <Button>Hello</Button>
+    </App>,
     document.getElementById("root")
 );
