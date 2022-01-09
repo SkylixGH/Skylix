3b18de511c3ae46caaa013c673b2e7604ef79ee3
XFaon <xfaonae@protonmail.com>
2022-01-09 12:33:15 -0500

    Basic desktop app

--- desktop/src/back/_main.js
+++ desktop/src/back/_main.js
@@ -1,5 +1,6 @@
 require("ts-node").register({
     transpileOnly: true,
     compilerOptions: { module: "CommonJS", esModuleInterop: true },
+    project: require("path").join(__dirname, "./tsconfig.json")
 });
 require("./main.ts");
--- desktop/src/front/main.tsx
+++ desktop/src/front/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App, Button, theming, themingThemes } from "@skylixgh/luxjs-client";
+import Root from "./Root";
 
 theming.installTheme(themingThemes.defaultDarkTheme).then(() => {
     theming.loadTheme(themingThemes.defaultDarkTheme.author, themingThemes.defaultDarkTheme.name);
 });
 
 ReactDOM.render(
-    <App>
-        <Button>Hello</Button>
-    </App>,
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
     document.getElementById("root")
 );
